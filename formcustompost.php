d1ec8d8665fe021f51231aa21259e94d903883af
Joao Vitor Pinto <jpinto@rtm.quebec>
2022-07-03 09:51:19 -0400

    replace plugin properties

--- formcustompost.php
+++ formcustompost.php
@@ -1,15 +1,13 @@
 /**
  * Plugin Name:       Form custom post
- * Plugin URI:        https://example.com/plugins/the-basics/
- * Description:       Handle the basics with this plugin.
- * Version:           1.10.3
+ * Plugin URI:        https://github.com/jvgpinto/formcustompost
+ * Description:       Create a custom post type with a form that can create a post from the front-end.
+ * Version:           0.0.1
  * Requires at least: 5.2
  * Requires PHP:      7.2
- * Author:            John Smith
- * Author URI:        https://author.example.com/
+ * Author:            Joao Pinto
+ * Author URI:        https://github.com/jvgpinto
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Update URI:        https://example.com/my-plugin/
- * Text Domain:       my-basics-plugin
- * Domain Path:       /languages
+ * Update URI:        https://github.com/jvgpinto/formcustompost
  */
