4e5ca333dcba8428543690c4ad81ba0ccf18e66e
Joao Vitor G Pinto <jvgpinto@gmail.com>
2022-07-15 16:22:41 -0400

    fix slug and title

--- functions/filters.php
+++ functions/filters.php
@@ -5,14 +5,21 @@ function filter_content_custompost ( $content ) {
             if ( shortcode_exists( get_option('custompost_field_shortcode_end')) ) {
                 $content .= do_shortcode('['.get_option('custompost_field_shortcode_end').']');
             }
-            return '<p>'.get_option('custompost_field_content').'</p> '.$content;
+            return '<p class="custompost">'.get_option('custompost_field_content').'</p> '.$content;
     }   
     return $content;
 }
 
-function filter_title_custompost ( $title ) {    
-    if (is_single() AND get_post_type( get_the_ID() ) == 'custompost' ) {
-            return esc_html__(get_option('custompost_field_title')).' '.$title;
-    }   
+function filter_title_custompost ( $title, $id = null ) {    
+
+    if ( ! is_admin() && ! is_null( $id ) ) {
+        $post = get_post( $id );
+        if ( $post instanceof WP_Post && ( $post->post_type == 'custompost') ) {
+            $new_titile = esc_html__(get_option('custompost_field_title','')).' '.$title;
+            if( ! empty( $new_titile ) ) {
+                return $new_titile;
+            }
+        }
+    }
     return $title;
 }
